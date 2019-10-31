31cd398634170950b60116287b424963e1e24b45
Polina Lyubeznova <goodbad8002@gmail.com>
2019-10-31 15:42:31 +0300

    Fetch

---
+++ script.js
@@ -0,0 +1,59 @@
+let url = window.location.toString();
+
+let getUserName = (url) => {
+  let getName = url.split("=");
+  let logName = getName[1];  
+  if (logName == undefined){
+    logName = "PaulinaLyu";
+  }
+   return logName;
+}
+
+let logName = getUserName(url);
+console.log(logName);
+
+fetch(`https://api.github.com/users/${logName}`)
+  .then(res => res.json())
+  .then(json => {
+    avatar = json.avatar_url;
+    name = json.name;
+    bio = json.bio;
+    url = json.url;
+
+    let modifyName = () => {
+      let userName = document.getElementsByTagName("h1");
+      userName[0].innerHTML = name;
+    }
+
+    let modifyBio = () => {
+      let userBio = document.getElementById("bio");
+      userBio.innerHTML = bio;
+      
+    }
+    let modifyAvatar = () => {
+      let userAvatar = document.getElementsByTagName("img");
+      userAvatar[0].src = avatar;
+      userAvatar[0].alt = "Avatar";
+    }
+    let modifyUrl = () => {
+      let userUrl = document.getElementById("url");
+      userUrl.href = `https://github.com/${logName}`;
+      
+      
+    }
+    modifyName();
+    modifyBio();
+    modifyAvatar();
+    modifyUrl();
+  })
+  .catch(err => document.write("Информация о пользователе не доступна"));
+
+  
+ 
+
+
+
+
+   
+
+   
