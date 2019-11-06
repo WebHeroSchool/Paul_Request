7331f75f28d66f7ae9ce6bf5fdb4eb84c20d503a
Polina Lyubeznova <goodbad8002@gmail.com>
2019-11-06 22:21:50 +0300

    alert

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let url =  window.location.toString();
+let url =  https://api.github.com/users=праороапроапж;
 
 let getUserName = (url) => {
   let getName = url.split("=");
@@ -17,27 +17,32 @@ fetch(`https://api.github.com/users/${logName}`)
     name = json.name;
     bio = json.bio;
     url = json.url;
-    let modifyName = () => {
-      let userName = document.getElementsByTagName("h1");
-      userName[0].innerHTML = name;
+    if (name) {
+      let modifyName = () => {
+        let userName = document.getElementsByTagName("h1");
+        userName[0].innerHTML = name;
+      }
+      let modifyBio = () => {
+        let userBio = document.getElementById("bio");
+        userBio.innerHTML = bio;
+      }
+      let modifyAvatar = () => {
+        let userAvatar = document.getElementsByTagName("img");
+        userAvatar[0].src = avatar;
+        userAvatar[0].alt = "Avatar";
+      }
+      let modifyUrl = () => {
+        let userUrl = document.getElementById("url");
+        userUrl.href = `https://github.com/${logName}`;
+      }
+      modifyName();
+      modifyBio();
+      modifyAvatar();
+      modifyUrl();
+    } else {
+      alert("Профиль не найден");
     }
-    let modifyBio = () => {
-      let userBio = document.getElementById("bio");
-      userBio.innerHTML = bio;
-    }
-    let modifyAvatar = () => {
-      let userAvatar = document.getElementsByTagName("img");
-      userAvatar[0].src = avatar;
-      userAvatar[0].alt = "Avatar";
-    }
-    let modifyUrl = () => {
-      let userUrl = document.getElementById("url");
-      userUrl.href = `https://github.com/${logName}`;
-    }
-    modifyName();
-    modifyBio();
-    modifyAvatar();
-    modifyUrl();
+    
   })
   .catch(err => document.write(err + "Информация о пользователе не доступна"));
 
